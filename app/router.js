32329f235bcdb5c7d778fc9379cfe939535d6c92
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-12 14:09:40 -0300

    adding order resource

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('orders', { path: '/orders' });
+    this.route('orders');
+    this.route('order', { path: '/orders/:order_id' });
 });
 export default Router;
