765bbaed0402b3d365ad0b6d469939614544bc71
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-16 16:33:09 -0300

    Adding bulk order feature

--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     actions: {
+        addToItemQuantity(lineItem, amount) {
+            lineItem.incrementProperty('quantity', amount);
+        },
+
         createOrder(order) {
             this.get('store').saveOrder(order);
             this.transitionTo('orders.order', order);
