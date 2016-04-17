ba4b774b461712189cf7f51182f57767ba091b8b
Juraci Vieira <juraci.vieira@gmail.com>
2016-04-17 15:29:33 -0300

    Adding an action to the component

--- app/components/item-percentage.js
+++ app/components/item-percentage.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        toggleDetails() {
+            this.toggleProperty('showDetails');
+        }
+    },
+
     isImportant: Ember.computed.gte('percentage', 50),
 
     percentage: Ember.computed('itemPrice', 'orderTotal', function() {
